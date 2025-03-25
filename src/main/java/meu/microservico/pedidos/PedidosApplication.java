a3bdb6fb3923795a3b29b05adf2394403061dbc7
fernando.bonetti <FernandoFBonetti@hotmail.com>
2025-03-25 14:30:52 -0300

    Initial commit

---
+++ src/main/java/meu/microservico/pedidos/PedidosApplication.java
@@ -0,0 +1,13 @@
+package meu.microservico.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+}
