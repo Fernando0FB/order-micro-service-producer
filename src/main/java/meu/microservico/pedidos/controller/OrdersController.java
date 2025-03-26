cdd7a567fc01b7063bd2f8d0e7cf457f13d0c918
fernando.bonetti <FernandoFBonetti@hotmail.com>
2025-03-26 09:25:50 -0300

    feat: configuring rabbitMQ routes and broker queue

--- src/main/java/meu/microservico/pedidos/controller/OrdersController.java
+++ src/main/java/meu/microservico/pedidos/controller/OrdersController.java
@@ -3,6 +3,7 @@ package meu.microservico.pedidos.controller;
 import meu.microservico.pedidos.model.Orders;
 import meu.microservico.pedidos.service.OrderService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,11 +20,14 @@ public class OrdersController {
         this.orderService = orderService;
     }
 
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
     @PostMapping
     public String createOrder(@RequestBody Orders order) {
         Orders savedOrder = orderService.saveOrder(order);
-        rabbitTemplate.convertAndSend("orders", savedOrder);
-        return "Order successfully saved and is now being processed: " + order.getDescription();
+        rabbitTemplate.convertAndSend("", routingKey, savedOrder.getDescription());
+        return "Order successfully saved and is now being processed: " + savedOrder.getDescription();
     }
 
     @GetMapping
